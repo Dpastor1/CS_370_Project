32eb86230ddce6f1067621b62602e49185a39113
Ashton J <ashj04@colostate.edu>
2024-04-03 14:23:10 -0600

    updated website to take two bounds, and outputs text based on average being within bounds

--- Website Files/script/index.js
+++ Website Files/script/index.js
@@ -1,11 +1,43 @@
-const moistureButton=document.querySelector("#moistureUpdate");
-const textentry=document.querySelector("#tbuser");
+const buttonLower=document.querySelector("#buttonLower");
+const buttonUpper=document.querySelector("#buttonUpper");
+const textentryLower=document.querySelector("#tbuser");
+const textentryUpper=document.querySelector("#upper")
 const output=document.querySelector("#output");
-let moistureAverage;
-let moistureCutoff;
-function updateMoistureCutoff(){
-    moistureAverage=textentry.value;
-    output.innerHTML = " " + moistureAverage;
+const conditionaltext=document.querySelector("#shouldWater");
+let moistureAverage=0;
+let moistureLower;
+let moistureUpper;
+//Logic for telling the user if they need to water their plant based on the moisture average over time
+const checkCutoff=()=>{
+    if(moistureLower>moistureUpper){
+        conditionaltext.innerHTML="Invalid Bounds, please make lower is less than or equal to the upper bounds";
+        return; 
+    }
+    if (moistureAverage > moistureUpper){
+        conditionaltext.innerHTML="Plant too humid! Water less";
+    }
+    else if (moistureAverage < moistureLower){
+        conditionaltext.innerHTML="Plant too dry! Water more";
+    }
+    else{
+        conditionaltext.innerHTML="Plant within humidity range, no need to water.";
+    }
 }
-moistureButton.addEventListener('click',updateMoistureCutoff);
+const updateLowerBound = ()=>{
+    moistureLower=textentryLower.value;
+    textentryLower.value="";
+    output.innerHTML = " " + moistureLower +" to "+moistureUpper;
+    checkCutoff();
+    //Send value to Pi
+}
+const updateUpperBound = ()=>{
+    moistureUpper=textentryUpper.value;
+    textentryUpper.value="";
+    output.innerHTML = " " + moistureLower +" to "+moistureUpper;
+    checkCutoff();
+    //Send value to Pi
+}
+
+buttonLower.addEventListener('click',updateLowerBound);
+buttonUpper.addEventListener('click',updateUpperBound);
 //Once Server is set up, send values to user side, page will load with values given, and display appropriate information
